9d67fcef58d47d545d0d2522b2646e108093fcaf
syaker <60866313+syaker@users.noreply.github.com>
2020-03-29 17:47:46 -0500

    Update changes

--- src/data.js
+++ src/data.js
@@ -1,5 +1,41 @@
-// estas funciones son de ejemplo
+import champs from './data/lol/lol.js';
 
-export const example = () => 'example';
+const dataChampions = champs.data;
+const arrayObjects = Object.keys(dataChampions);
+const list = document.getElementById('orderList');
+list.addEventListener('change', () => {
+  let saveDates = '';
+  const valor = document.getElementById('orderList').value;
+  if (valor === 'az') {
+    const upward = arrayObjects.sort((a, b) => a.localeCompare(b));
+    upward.forEach((z) => {
+      const name = `<p class='info'>${dataChampions[z].name}</p>`;
+      const roleId = `<p class ='info2'>${dataChampions[z].tags}</p>`;
+      const attack = `<p class='info2'>Nivel de ataque: ${dataChampions[z].info.attack}</p>`;
+      const defense = `<p  class='info2' >Nivel de defensa: ${dataChampions[z].info.defense}</p>`;
+      saveDates += `
+        <section class='target'style='background:url(${dataChampions[z].splash}); background-size: 100% 100%'>
+        <div id='Datescard'>
+        ${name + roleId + attack + defense} 
+        </div>
+        </section>
+        `;
+    });
+    document.querySelector('#champions').innerHTML = saveDates;
+  } else {
+    const falling = arrayObjects.sort((a, b) => b.localeCompare(a));
 
-export const anotherExample = () => 'OMG';
+    falling.forEach((m) => {
+      const name = `<p class='info' > ${dataChampions[m].name}</p > `;
+      const roleId = `<p class ='info2' > ${dataChampions[m].tags}</p > `;
+      const attack = `<p class ='info2' > Nivel de ataque: ${dataChampions[m].info.attack}</p > `;
+      const defense = `<p  class ='info2' > Nivel de defensa: ${dataChampions[m].info.defense}</p > `;
+      saveDates += `<section class ='target' style = 'background: url(${dataChampions[m].splash}); background-size: 100% 100%' >
+            <div id='Datescard'>
+              ${name + roleId + attack + defense}
+            </div>
+    </section >`;
+    });
+    document.querySelector('#champions').innerHTML = saveDates;
+  }
+});
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import champs from './data/lol/lol.js';
 
-console.log(example, data);
+let space = '';
+const dataChamps = champs.data;
+const showAll = () => {
+  for (let properties in dataChamps) {
+    let nameId = `<p class='info'>${dataChamps[properties].name}</p>`;
+    let roleId = `<p class ='info2'>${dataChamps[properties].tags}</p>`;
+    let attack = `<p class='info2'>Nivel de ataque: ${dataChamps[properties].info.attack}</p>`;
+    let defense = `<p  class='info2' >Nivel de defensa: ${dataChamps[properties].info.defense}</p>`;
+    space += `
+    <section class='target'style="background:url(${dataChamps[properties].splash}); background-size: 100% 100%">
+    <div id='Datescard'>
+    ${nameId + roleId + attack + defense} 
+    </div>
+    </section>
+    `;
+  }
+};
+
+showAll(champs);
+document.getElementById('champions').innerHTML = space;
