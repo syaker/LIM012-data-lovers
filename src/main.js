27793dbec8d2094d0586d8fd3d887b5aa67af6de
syaker <60866313+syaker@users.noreply.github.com>
2020-03-29 17:55:55 -0500

    Update changes 2

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import champs from './data/lol/lol.js';
 let space = '';
 const dataChamps = champs.data;
 const showAll = () => {
-  for (let properties in dataChamps) {
-    let nameId = `<p class='info'>${dataChamps[properties].name}</p>`;
-    let roleId = `<p class ='info2'>${dataChamps[properties].tags}</p>`;
-    let attack = `<p class='info2'>Nivel de ataque: ${dataChamps[properties].info.attack}</p>`;
-    let defense = `<p  class='info2' >Nivel de defensa: ${dataChamps[properties].info.defense}</p>`;
+  for (const properties in dataChamps) {
+    const nameId = `<p class='info'>${dataChamps[properties].name}</p>`;
+    const roleId = `<p class ='info2'>${dataChamps[properties].tags}</p>`;
+    const attack = `<p class='info2'>Nivel de ataque: ${dataChamps[properties].info.attack}</p>`;
+    const defense = `<p  class='info2' >Nivel de defensa: ${dataChamps[properties].info.defense}</p>`;
     space += `
     <section class='target'style="background:url(${dataChamps[properties].splash}); background-size: 100% 100%">
     <div id='Datescard'>
