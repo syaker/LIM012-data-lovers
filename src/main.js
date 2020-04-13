81c48a0b802bd51ed3ee78d13d61efc615ae9bdb
syaker <60866313+syaker@users.noreply.github.com>
2020-04-13 12:19:48 -0500

    Update 13/04/20 12:19

--- src/main.js
+++ src/main.js
@@ -132,3 +132,16 @@ inputSearch.addEventListener('input', () => {
     listChampReverse(cardReverse);
   }
 });
+/*
+// HAMBURGUER MENU
+const menu = document.querySelector('.hamburger');
+
+// method
+function toggleMenu(event) {
+  this.classList.toggle('is-active');
+  document.querySelector('.menupal').classList.toggle('is_active');
+  event.preventDefault();
+}
+// event
+menu.addEventListener('click', toggleMenu, false);
+// rotateY(180deg) */
